6ac15ff7f10fdbe257afa67dcb2f7535185b1abc
Ryunosuke Shigematsu <142504047+RyunosukeShigematsu@users.noreply.github.com>
2026-02-09 13:13:27 +0900

    Modify allowed origins in time.php
    Updated allowed origins for CORS to include a placeholder domain.

--- server/api/time.php
+++ server/api/time.php
@@ -3,9 +3,7 @@
 header('Content-Type: application/json; charset=UTF-8');
 
 $allowedOrigins = [
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'https://shigematsu.nkmr.io',
+  'https://YOUR_DOMAIN.example',
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
