41fc7fd94066d3556f044e73e463e569bc609e39
Ryunosuke Shigematsu <142504047+RyunosukeShigematsu@users.noreply.github.com>
2026-02-09 13:13:43 +0900

    Modify CORS allowed origins in upload_audio.php
    Updated allowed origins for CORS in upload_audio.php.

--- server/api/upload_audio.php
+++ server/api/upload_audio.php
@@ -7,9 +7,7 @@ header('Content-Type: application/json; charset=UTF-8');
  * ※必要に応じて localhost:3000 だけ許可にしてる
  */
   $allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://shigematsu.nkmr.io',
+  'https://YOUR_DOMAIN.example',
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
