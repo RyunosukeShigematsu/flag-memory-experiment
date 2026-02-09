f2f29363980a8902da7dfb835dbc4013da113888
Ryunosuke Shigematsu <142504047+RyunosukeShigematsu@users.noreply.github.com>
2026-02-09 13:13:55 +0900

    Update upload_flag.php

--- server/api/upload_flag.php
+++ server/api/upload_flag.php
@@ -6,9 +6,7 @@ header('Content-Type: application/json; charset=UTF-8');
  * CORS（audio.phpと同じ流儀）
  */
 $allowedOrigins = [
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'https://shigematsu.nkmr.io',
+  'https://YOUR_DOMAIN.example',
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
